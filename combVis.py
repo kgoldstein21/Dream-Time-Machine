5684d2434f2b5370df50efadda51fc3f1a93c65a
Annie Tor <ator@olin.edu>
2019-12-11 13:59:34 -0500

    adding documentation for combVis

--- combVis.py
+++ combVis.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
+
 #visual
 import numpy as np
 import cv2
 import time
 from PIL import Image, ImageFont, ImageDraw
-#music
 
+#music
 import librosa
 import os
 from pygame import mixer  # Load the popular external library
@@ -16,13 +17,8 @@ import sys
 
 import ctypes
 
+#initializing video capture and facial recognition variables and setting default mode and song
 cap = cv2.VideoCapture(0)
-# if not cap.isOpened():
-#     raise Exception('could not open video device')
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-# ret, frame = cap.read()
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 kernel = np.ones((21, 21), 'uint8')
 start_time = time.time()
@@ -34,12 +30,26 @@ mode="happy"
 song="super"
 
 def background(frame, image_name):
+    '''
+    Takes in a frame image and an image name string.
+
+    Matches the size of the image to the size of the frame.
+
+    Returns the resized image.
+    '''
     rows,cols,channels = frame.shape
     img1 = cv2.imread(image_name)
     resized_image = cv2.resize(img1, (cols, rows))
     return resized_image
 
 def add_images(frame, image_name):
+    '''
+    Takes in a frame image and an image name string.
+
+    Finds outlines in the frame and superimposes those on top of the image belonging to the image name.
+
+    Returns the final composite image.
+    '''
     frame = cv2.Canny(frame, 50, 100)
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
     blue, green, red = cv2.split(frame)
@@ -53,6 +63,15 @@ def play_song(file_name):
     mixer.music.play()
 
 def combinedVisual(cap, face_cascade, kernel, start_time, more_shapes, counter, corpcounter, dir, mode, song):
+    '''
+    Takes in a video capture, an image cascade, a kernel, a start time, a boolean more_shapes,
+    a counter, another counter, a boolean dir, a mode name string, and a song name string.
+
+    Does all the heavy lifting for the video and audio ouput-- plays the selected song, opens
+    up a video feed, draws circles over each detected face to the beat of the song, chooses a
+    background from the frames_and_modes folder based on the chosen mode, shows everything in a
+    window, and then closes everything when you hit the 'q' key.
+    '''
     play_song('playlist/'+song+'.mp3')
     song_start_time = time.time()
     tempo = int(603)
@@ -64,18 +83,12 @@ def combinedVisual(cap, face_cascade, kernel, start_time, more_shapes, counter, 
         #Capturing frames from the video feed
         ret, frame = cap.read()
         song_elapsed_time = 1000*(time.time()-song_start_time)
-        #print("song elapsed time: " + song_elapsed_time)
-        #print("tempo: " + tempo)
 
         #Drawing pulsing circles from each detected face
         faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20, 20))
         for (x, y, w, h) in faces:
             elapsed_time = time.time()-start_time
-            print(elapsed_time)
-            #cv2.circle(frame, (x+2*int(w/4), y+int(h/3)),int(w/20)*int(300**elapsed_time), (255,255,255), thickness=1, lineType=8, shift=0)
             if song_elapsed_time > threshold:
-                print('in the loop')
-                print(wid)
                 threshold += tempo
                 if wid < 50:
                     wid += 10
@@ -90,7 +103,7 @@ def combinedVisual(cap, face_cascade, kernel, start_time, more_shapes, counter, 
                 more_shapes = True
 
 
-        #Adding background image
+        #Adding background image based on selected mode
         if mode == "corporate":
             if counter >= 0 and counter < 66:
                 if corpcounter < 15:
@@ -131,8 +144,6 @@ def combinedVisual(cap, face_cascade, kernel, start_time, more_shapes, counter, 
                 else:
                     resized_image = background(frame, 'frames_and_modes/'+mode+'/000'+str(counter)+'.jpg')
                     dir = True
-
-
 
         frame = add_images(frame, resized_image)
 
